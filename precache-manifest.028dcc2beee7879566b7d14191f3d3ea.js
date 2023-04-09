f50794817ca7f52fc16f956ceccec2d256befb1e
Jack Marchant <jackmarchantt@gmail.com>
2023-04-09 15:54:19 -0400

    Updates

---
+++ precache-manifest.028dcc2beee7879566b7d14191f3d3ea.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e91a0f4e4f42f31e16a",
+    "url": "/Portfolio-React/static/css/main.6ec702a5.chunk.css"
+  },
+  {
+    "revision": "6e91a0f4e4f42f31e16a",
+    "url": "/Portfolio-React/static/js/main.6e91a0f4.chunk.js"
+  },
+  {
+    "revision": "03c018fb6e914630103e",
+    "url": "/Portfolio-React/static/js/1.03c018fb.chunk.js"
+  },
+  {
+    "revision": "7fff9feb6bd6e71d0511",
+    "url": "/Portfolio-React/static/js/2.7fff9feb.chunk.js"
+  },
+  {
+    "revision": "ab038ea13640244b9701",
+    "url": "/Portfolio-React/static/js/runtime~main.ab038ea1.js"
+  },
+  {
+    "revision": "bc08838ff96c58e0e499925e3113143e",
+    "url": "/Portfolio-React/static/media/pub-golf-banner.bc08838f.png"
+  },
+  {
+    "revision": "f9ea451c9a443d3dffe777b555c5c387",
+    "url": "/Portfolio-React/static/media/Resume_Portfolio.f9ea451c.pdf"
+  },
+  {
+    "revision": "f8d48d6e399432fb9d2ec4317e4477dd",
+    "url": "/Portfolio-React/static/media/mail-logo.f8d48d6e.png"
+  },
+  {
+    "revision": "0c673f5aa02d70eb085fe3d35897bc5c",
+    "url": "/Portfolio-React/static/media/johnston-itc-std-bold-webfont.0c673f5a.woff2"
+  },
+  {
+    "revision": "1b788ec967d971198ad09e705b7fdf73",
+    "url": "/Portfolio-React/static/media/johnstonitcstd-medium-webfont.1b788ec9.woff"
+  },
+  {
+    "revision": "d60e2151ae830d83a23226cbf57f543c",
+    "url": "/Portfolio-React/static/media/johnston-itc-std-bold-webfont.d60e2151.woff"
+  },
+  {
+    "revision": "c10964129c4240447b599f65a4500716",
+    "url": "/Portfolio-React/static/media/johnstonitcstd-medium-webfont.c1096412.woff2"
+  },
+  {
+    "revision": "8666e4982a273ead0f081ebaed4729d1",
+    "url": "/Portfolio-React/index.html"
+  }
+];
