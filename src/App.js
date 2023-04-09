a75359d297a87b1fd265ab368be42ea037dcc3a5
Jack Marchant <jackmarchantt@gmail.com>
2023-04-09 16:13:53 -0400

    Changed to HashRouter to accomodate for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import Homepage from './components/Homepage';
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Section from './components/Section';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
           <Routes>
             <Route path="/" element={<Homepage />} />
             <Route path="/projects" element={<Section section="proj" startNum={0} />} />
             <Route path="/contact" element={<Section section="cont" startNum={100} />} />
             <Route path="/about" element={<Section section="about" startNum={200} />} />
           </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
